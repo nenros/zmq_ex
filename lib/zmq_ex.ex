cb460f5e64c34aa03ed890498b1797784261fe31
Fabian Krol <fabian@krol.io>
2018-07-25 20:30:31 +0200

    wip2

--- lib/zmq_ex.ex
+++ lib/zmq_ex.ex
@@ -34,8 +34,8 @@ defmodule ZmqEx do
 
   def recv(socket) do
     {:ok, msg} = :gen_tcp.recv(socket, 0)
-    dmsg = decode msg
     IO.inspect msg
+    dmsg = decode msg
     IO.inspect dmsg
     # :gen_tcp.send(socket, msg)
     recv(socket)
@@ -43,9 +43,10 @@ defmodule ZmqEx do
 
   def decode(<<4, s :: size(8),
                 comm_size, comm :: binary - size(comm_size),
-                comm_size_2, comm_2 :: binary - size(comm_size_2),
-                0, 0, 0, x :: size(8),
-                body :: binary>>) do
-    {s, comm, comm_2, x, body}
-  end
+                comm_size_2, comm_2 :: binary - size(comm_size_2), 0, 0, 0,
+                comm_size_3, comm_3 :: binary - size(comm_size_3),
+                comm_size_4, comm_4 :: binary - size(comm_size_4),
+                body :: binary>>), do:
+    {s, comm, comm_2, comm_3, comm_4, body}
+  def decode(<<0, msg_size, msg :: binary - size(msg_size)>>), do: msg
 end
--- python_server.py
+++ python_server.py
@@ -2,10 +2,11 @@ import time
 import zmq
 
 context = zmq.Context()
-socket = context.socket(zmq.REP)
+socket = context.socket(zmq.DEALER)
 socket.bind("tcp://*:5555")
 
 while True:
     msg = input("Please enter something: ")
     #  Send reply back to client
     socket.send_string(msg)
+    # socket.send(b"WORLD\r\n")
